42b8f3988e411eae19f8846d7d74e709529225c2
rasel_virus <niloyahmedrasel24@gmail.com>
2023-12-26 09:05:05 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './App.css';
+import Login from './Login/Login';
+import Main from './Main';
+import Signup from './Signup';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
+  const router =createBrowserRouter([
+    {
+      path: '/',
+      element:<Main></Main>,
+      children:[
+        {
+          path: '/login',
+          element:<Login></Login>,
+        },
+        {
+          path: '/',
+          element:<Signup></Signup>
+        }
+      ]
+    }
+  ])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Toaster></Toaster>
+    <RouterProvider router={router}></RouterProvider>
+      
     </div>
   );
 }
